8a785b2456935096855379819024ada84d06118e
iypod <wrof80@gmail.com>
2017-12-09 02:26:59 +0900

    初コミット

---
+++ script.R
@@ -0,0 +1,6 @@
+# RStudioではじめるGitによるバージョン管理
+# https://qiita.com/uri/items/6b94609f156173ed43ed
+
+# IT苦手な人のためのWindows+gitでRStudioによるバージョン管理入門
+# http://d.hatena.ne.jp/EulerDijkstra/20130327/1364392046/
+
